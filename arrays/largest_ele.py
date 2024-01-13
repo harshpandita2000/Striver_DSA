4648e06fcca1b21a2c8277c1d0cfb8876164aee2
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-13 10:09:29 +0530

    code for Largest element in an array

--- arrays/largest_ele.py
+++ arrays/largest_ele.py
@@ -1,5 +1,15 @@
 def find_largest_element(arr):
-    print(arr)
-    print("hello")
+    largest=arr[0]
+    for ele in arr:
+        if ele > largest:
+            largest=ele
+    print(largest)
+    return largest
 
-find_largest_element([1,3,4,22,11])
+find_largest_element([1,3,4,22,11]) 
+# print(largest_no)
+
+
+
+
+
