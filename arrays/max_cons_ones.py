e475098527386697b8c25c5a037acd1aad15a115
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-16 09:53:33 +0530

    optimised code

--- arrays/max_cons_ones.py
+++ arrays/max_cons_ones.py
@@ -13,4 +13,22 @@ def max_consecutive_one(arr):
     return max(temp)
 arr=[1,1,1,1,0,1,1,1,1,1]
 print(max_consecutive_one(arr))
+# TC:O(N)
+# SC:O(N) #WORST CASE WHEN ALL ELEMENTS R 0'S 
+def max_consecutive_1(arr):
+    cnt=0
+    maxi=0
+    for i in range(len(arr)):
+        if arr[i]==1:
+            cnt+=1
+        else:
+            cnt = 0
+    maxi = max(maxi, cnt)
+    return maxi
+arr=[1,1,1,1,0,1,1,1,1,1,1]
+print(max_consecutive_1(arr))
+# TC:O(N)
+# SC:O(1)
+    
+
 
