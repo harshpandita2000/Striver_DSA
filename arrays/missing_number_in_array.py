34499ff2ad33cf16c000dd92190d4a9f29555e7a
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-16 14:16:30 +0530

    optimised code..

--- arrays/missing_number_in_array.py
+++ arrays/missing_number_in_array.py
@@ -1,9 +1,20 @@
 
-def missing_number(arr):
-   for i in range(0,len(arr)+1):
+def missing_number_for_sorted(arr):
+   for i in range(0,len(arr)): # 1 3 4 5
       if arr[i]!= i + 1: 
         return i + 1
-arr=[1,3]
+      
+arr=[1,11]
+print(missing_number_for_sorted(arr))
+
+def missing_number(arr):
+   n = max(arr)
+   a = sum(arr)
+   s = (n*(n+1))//2
+   return s - a
+arr=[1,3,4]
 print(missing_number(arr))
+
+
 
       
