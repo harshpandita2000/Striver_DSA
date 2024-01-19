4b8ec028dc763595960726039fc9d8b9e93c9bd0
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-19 20:32:39 +0530

    bruteforce

--- arrays/sub_array_with_sum_k.py
+++ arrays/sub_array_with_sum_k.py
@@ -1,4 +1,3 @@
-
 #2nd optimisation avoid using temp arr
 def sum_k(arr, k):
     arr_sum = 0
@@ -12,41 +11,19 @@ def sum_k(arr, k):
         while j < n:
             arr_sum += arr[j]
             if arr_sum == k:
-               # sub_arr.append(temp)
                 leng=max(leng, j - i + 1)
             j += 1
         i += 1
     print(leng)
     
 #arr = [17, 0, 0, 0, 0, 17, 0, 0]
-arr = [17, 0, 0, 0, 0, 17, 0, 0, -1,1]
-sum_k(arr, 17)
-#TC:O(N) #BECAUSE  inner loop efficiently traverses the array only once,while outer loop runs n times
+# arr = [17, 0, 0, 0, 0, 17, 0, 0, -1,1]
+#arr=[3,-1,-2,-4,4,0]
+arr=[0, 1 ,1, -1 , 0,-1]
+
+sum_k(arr, 0)
+#TC:O(N^2) #BECAUSE  inner loop efficiently traverses the array only once,while outer loop runs n times
 #SC:O(1)
-
-
-#sliding wind approach
-def sum_k_optimized(arr, k):
-    n = len(arr)
-    arr_sum = 0
-    start = 0
-    leng = 0
-
-    for end in range(n):
-        arr_sum += arr[end]
-
-        while arr_sum > k:
-            arr_sum -= arr[start]
-            start += 1
-
-        if arr_sum == k:
-            leng = max(leng, end - start + 1)
-
-    return leng
-arr=[8, 15, 17, 0, 11]
-print(sum_k_optimized(arr, 17))
-
-
 
 
 
