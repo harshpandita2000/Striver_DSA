dbddfa48bfb2b227bd1e46d8f0eefd0b88ccf7ec
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-20 20:59:47 +0530

    dnf algo

--- arrays/medium/sort_0_1_2.py
+++ arrays/medium/sort_0_1_2.py
@@ -8,5 +8,27 @@ def sort(arr):
 # arr=[1,0,0,1,2,1]
 arr=[0,0,0,1,2,1,1,0]
 print(sort(arr))
+# tc:O(N^2)
+# sc:O(1)
+#DNF ALGO APPROACH
+def sort(arr):
+    n=len(arr)
+    low=0
+    mid=0
+    high=n-1
+    while mid<=high:
+        if arr[mid]==0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low+=1
+            mid+=1
+        elif arr[mid]==1:
+            mid+=1
+        else:
+            arr[mid],arr[high]=arr[high],arr[mid]
+            high-=1
+    return(arr)
 
-
+arr=[0,0,0,1,2,1,1,0]
+print(sort(arr))
+# TC:O(N)
+# SC:O(1)
