84d826c8d60b34fc76bdd7109666f88b8fd138bd
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-22 23:22:38 +0530

    one more approach

--- arrays/medium/sort_0_1_2.py
+++ arrays/medium/sort_0_1_2.py
@@ -15,8 +15,8 @@ def sort(arr):
     n=len(arr)
     low=0
     mid=0
-    high=n-1
-    while mid<=high:
+    high=n-1                                                      
+    while mid<=high:                                      
         if arr[mid]==0:
             arr[low],arr[mid]=arr[mid],arr[low]
             low+=1
@@ -28,7 +28,34 @@ def sort(arr):
             high-=1
     return(arr)
 
-arr=[0,0,0,1,2,1,1,0]
+arr=[0,2,0,1]
 print(sort(arr))
 # TC:O(N)
 # SC:O(1)
+
+
+def sort(arr):
+    n = len(arr)
+    arr2 = []
+    cnt0 = 0
+    cnt1 = 0
+    cnt2 = 0
+    for i in range(0,n):
+        if arr[i] == 0:
+            cnt0 += 1
+        elif arr[i] == 2:
+            cnt2 +=1
+        else:
+            cnt1 +=1
+    #print(cnt0,cnt1,cnt2)
+    for i in range(cnt0):
+        arr2.append(0)
+    for i in range(cnt1):
+        arr2.append(1)
+    for i in range(cnt2):
+        arr2.append(2)
+    
+    return(arr2)
+
+arr=[0,0,0,1,2,1,1,0]
+print(sort(arr))
