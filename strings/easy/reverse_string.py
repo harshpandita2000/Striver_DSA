c66f71b2fd0459f23efce03b1e1629c1f476edbb
harshpandita2000 <panditaharsh32@gmail.com>
2024-01-25 23:53:05 +0530

    reverse string

--- strings/easy/reverse_string.py
+++ strings/easy/reverse_string.py
@@ -1,6 +1,8 @@
 #this is aparna
 s = "this is aparna"
-string = s.split(" ")
+string = s.strip().split()
 reverse = string[::-1] 
 print(reverse)
 print(" ".join(reverse))
+ or 
+#  return " ".join(s.strip().split()[::-1])
