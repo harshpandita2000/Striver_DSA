6049451ff990631c03dfba6041ac0d11def5ce7e
Harsh Pandita <55092085+harshpandita2000@users.noreply.github.com>
2024-01-30 10:14:08 +0530

    Delete arrays directory

--- arrays/easy/left_rotate_array.py
+++
@@ -1,9 +0,0 @@
-def left_rotate_array_by_one(arr):
-    temp=arr[0]
-    for i in range(len(arr)-1):
-        arr[i]=arr[i+1]
-    arr[len(arr)-1]=temp
-    return arr
-
-arr=[1,2,3,4,5]
-print(left_rotate_array_by_one(arr))
--- arrays/easy/linear_search.py
+++
@@ -1,9 +0,0 @@
-def linear_search(arr):
-    for i in range(0,len(arr)):
-        if arr[i]==item:
-            return i
-    return -1
-    
-arr=[1,4,7,5]
-item=6
-print(linear_search(arr))
