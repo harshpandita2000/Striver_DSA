ed1a567af630332dc7f8d90ab8dda544891d5c1d
harshpandita2000 <panditaharsh32@gmail.com>
2024-02-03 22:04:02 +0530

    ..

--- arrays/easy/linear_search.py
+++ arrays/easy/linear_search.py
@@ -7,3 +7,20 @@ def linear_search(arr):
 arr=[1,4,7,5]
 item=6
 print(linear_search(arr))
+########### java code
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 5};
+        int item = 4;
+        System.out.println(linearSearch(arr, item));
+    }
+}
